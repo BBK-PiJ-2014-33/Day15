248dab1cad696744283d16cc953714fea35e84f9
BBK-PiJ-2014-33 <ludak303@gmail.com>
2015-01-26 18:41:33 +0000

    Finished Exercise 1

---
+++ src/TestClass.java
@@ -0,0 +1,12 @@
+/**
+ * Created by lulu on 26/01/2015.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        CodeFlow myCodeFlow = new CodeFlow();
+        myCodeFlow.launch(0);
+        myCodeFlow.launch(2);
+        myCodeFlow.launch(4);
+        myCodeFlow.launch(6);
+    }
+}
